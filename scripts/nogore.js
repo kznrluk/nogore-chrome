a091f55ce043f8bd04c3878dd6b264466d4f8612
kznrluk <kznr.luk@gmail.com>
2018-01-28 19:26:30 +0900

    Delete debug code

--- scripts/nogore.js
+++ scripts/nogore.js
@@ -1,22 +1,15 @@
-'use strict';
-
-const targetWords = 'グロ';
-// const target = document.getElementById('permalink-overlay');
+const targetWords = 'このリプ欄にはグロ画像があります';
 const target = document.body;
 const observer = new MutationObserver(() => {
     if($('.permalink').length){
-        console.log('Open Permalink')
         checkWords();
     }
 });
 
 const checkWords = () => {
     $(function () {
-        console.log($('#permalink-overlay').find('.tweet-text'))
         for(var tweets of $('#permalink-overlay').find('.tweet-text')){
-            //console.log(tweets.innerHTML)
             if(tweets.innerHTML.indexOf(targetWords) !== -1){
-                console.log('HIT')
                 imgChanger();
             }
         }
@@ -25,9 +18,9 @@ const checkWords = () => {
 
 function imgChanger(){
     $(function () {
+        console.log('No Gore Twitter: Image Blocked!')
         $('#permalink-overlay').find('.AdaptiveMedia-photoContainer').find('img').css('display', 'none');
     });
 }
 
-checkWords();
 observer.observe(target,{childList: true, subtree: true})
