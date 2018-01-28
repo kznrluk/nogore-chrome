1d96931b1c970503bc9c73a5348503598124375b
kznrluk <kznr.luk@gmail.com>
2018-01-28 17:59:24 +0900

    DOMの再描写に対応

--- scripts/nogore.js
+++ scripts/nogore.js
@@ -1,18 +1,33 @@
 'use strict';
 
-const targetWords = 'グロ'
+const targetWords = 'グロ';
+// const target = document.getElementById('permalink-overlay');
+const target = document.body;
+const observer = new MutationObserver(() => {
+    if($('.permalink').length){
+        console.log('Open Permalink')
+        checkWords();
+    }
+});
 
-function imgChanger(){
+const checkWords = () => {
     $(function () {
-        $('.AdaptiveMedia-photoContainer').find('img').css('display', 'none');
+        console.log($('#permalink-overlay').find('.tweet-text'))
+        for(var tweets of $('#permalink-overlay').find('.tweet-text')){
+            //console.log(tweets.innerHTML)
+            if(tweets.innerHTML.indexOf(targetWords) !== -1){
+                console.log('HIT')
+                imgChanger();
+            }
+        }
     });
 }
 
-$(function () {
-    for(var tweets of $('.tweet-text')){
-        console.log(tweets.innerHTML)
-        if(tweets.innerHTML.indexOf(targetWords) !== -1){
-            imgChanger();
-        }
-    }
-});
+function imgChanger(){
+    $(function () {
+        $('#permalink-overlay').find('.AdaptiveMedia-photoContainer').find('img').css('display', 'none');
+    });
+}
+
+checkWords();
+observer.observe(target,{childList: true, subtree: true})
